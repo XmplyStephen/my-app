55d96fcbd0c887d67a92caf39aaa40a0ef09e189
Stephen's Book <stephen@acct047-pc.mutuallifeng.local>
2019-05-07 16:10:54 +0100

    A new test commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Learn to design and code React Apps</h1>
+    <h1>Learn to design and code React Apps Beginner</h1>
     <p>This is going to be an awesome first website. On the journey to building more. Leggo!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
